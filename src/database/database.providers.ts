2921fc27d238cfb431a1487a7964efca861f7a1e
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2021-11-28 08:53:59 +0200

    updating mongoose for connection on prodution

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,7 @@
-import * as mongoose from 'mongoose';
-
+import mongoos, { connect } from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.DATA_BASE),
+    useFactory: (): Promise<typeof mongoos> => connect(process.env.DATA_BASE),
   },
 ];
